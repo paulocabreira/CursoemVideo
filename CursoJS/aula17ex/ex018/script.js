1132569a6824f906b5426d1156027273d5db86d6
Paulo Cabreira <paulocabreira@gmail.com>
2023-11-30 20:56:54 -0300

    Exercícios JavaScript (Parte 8) - Curso JavaScript

--- CursoJS/aula17ex/ex018/script.js
+++ CursoJS/aula17ex/ex018/script.js
@@ -3,7 +3,8 @@ let htmRes = window.document.getElementById('resultado')
 
 function adicionar () {
 
-    let numero = Number(window.document.getElementById('inpNumero').value)
+    let inpnum = window.document.getElementById('inpNumero')
+    let numero = Number(inpnum.value)
     let htmSel = window.document.getElementById('selValores')
     let selOpt = document.createElement('option', 1)
 
@@ -17,25 +18,39 @@ function adicionar () {
         valores.push(numero)
         htmRes.innerHTML = ''
     }
+
+    inpnum.value = ''
+    inpnum.focus()
+
 }
 
 function finalizar () {
 
     let arrSoma = 0
     let arrMedia = 0
+    let maior = valores[0]
+    let menor = valores[0]
 
-    for (let i in valores) {
-        arrSoma += valores[i]
+    if (valores.length <= 0) {
+        alert('Adicione valores antes de finalizar!')
+    } else {
+
+        for (let i in valores) {
+            arrSoma += valores[i]
+
+            if (valores[i] > maior)
+                maior = valores[i]
+
+            if (valores[i] < menor)
+                menor = valores[i]            
+        }
+    
+        arrMedia = arrSoma / valores.length
+    
+        htmRes.innerHTML  = `<p>Ao todo, temos <strong>${valores.length}</strong> números cadastrados.</p>`
+        htmRes.innerHTML += `<p>O maior valor informado foi <strong>${maior}</strong>.</p>`
+        htmRes.innerHTML += `<p>O menor valor informado foi <strong>${menor}</strong>.</p>`    
+        htmRes.innerHTML += `<p>Somando todos os valores, temos <strong>${arrSoma}</strong>.</p>`
+        htmRes.innerHTML += `<p>A média dos valores digitados é <strong>${arrMedia.toFixed(2).replace('.',',')}</strong>.</p>`
     }
-
-    arrMedia = arrSoma / valores.length
-
-    valores.sort()
-
-    htmRes.innerHTML  = `<p>Ao todo, temos <strong>${valores.length}</strong> números cadastrados.</p>`
-    htmRes.innerHTML += `<p>O maior valor informado foi <strong>${valores[valores.length - 1]}</strong>.</p>`
-    htmRes.innerHTML += `<p>O menor valor informado foi <strong>${valores[0]}</strong>.</p>`    
-    htmRes.innerHTML += `<p>Somando todos os valores, temos <strong>${arrSoma}</strong>.</p>`
-    htmRes.innerHTML += `<p>A média dos valores digitados é <strong>${arrMedia.toFixed(2).replace('.',',')}</strong>.</p>`
-
 }
